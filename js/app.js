ebba2a8a5354b7ab8b0a21631cf03c7a588e659a
jbelloncordobes <jbelloncordobes@gmail.com>
2025-05-17 17:11:55 +0200

    Cambiadas " por ' por eslint

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleBtn = document.getElementById("toggleComparativa");
-    const alertBtn = document.getElementById("alertButton");
-    const themeBtn = document.getElementById("changeTheme");
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('toggleComparativa');
+    const alertBtn = document.getElementById('alertButton');
+    const themeBtn = document.getElementById('changeTheme');
 
     if (toggleBtn) {
-        toggleBtn.textContent = "Amaga/Mostra Taula";
-        toggleBtn.addEventListener("click", () => {
-            const table = document.getElementById("comparativa");
-            if (table.style.display === "none") {
-                table.style.display = "table";
+        toggleBtn.textContent = 'Amaga/Mostra Taula';
+        toggleBtn.addEventListener('click', () => {
+            const table = document.getElementById('comparativa');
+            if (table.style.display === 'none') {
+                table.style.display = 'table';
             } else {
-                table.style.display = "none";
+                table.style.display = 'none';
             }
         });
     }
 
     if (alertBtn) {
-        alertBtn.textContent = "Fes un alert";
-        alertBtn.addEventListener("click", () => {
-            alert("Això és una curiositat!");
+        alertBtn.textContent = 'Fes un alert';
+        alertBtn.addEventListener('click', () => {
+            alert('Això és una curiositat!');
         });
     }
 
     if (themeBtn) {
-        themeBtn.textContent = "Canvia el tema";
-        themeBtn.addEventListener("click", () => {
-            document.body.classList.toggle("dark-mode");
+        themeBtn.textContent = 'Canvia el tema';
+        themeBtn.addEventListener('click', () => {
+            document.body.classList.toggle('dark-mode');
         });
     }
 });
